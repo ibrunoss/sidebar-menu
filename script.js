3c201f315e350d8abb8362a3890511a16f65d579
Bruno Silva Santana <ibrunoss@outlook.com>
2021-10-22 20:45:33 -0300

    Feat: Open/closed side menu toggle

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const toggleMenu = document.querySelector(".toggle");
+const sideBarMenu = document.querySelector(".navigation");
+
+toggleMenu.onclick = () => {
+  toggleMenu.classList.toggle("active");
+  sideBarMenu.classList.toggle("active");
+};
+
 const list = document.querySelectorAll(".list");
 
 list.forEach((menuItem) => {
