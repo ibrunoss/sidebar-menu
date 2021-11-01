b1e3574b72edc7a15754ee5286f373feeda28296
Bruno Silva Santana <ibrunoss@outlook.com>
2021-11-01 19:23:28 -0300

    feat: apply Menu on App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import Menu from "./components/navigation/Menu/Menu";
+
 export default function App() {
-  return <div>Sidebar Menu Using, Html, CSS & JavaScript</div>;
+  return (
+    <div>
+      <h1>Sidebar Menu Using, Html, CSS & JavaScript</h1>
+      <Menu />
+    </div>
+  );
 }
