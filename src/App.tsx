1f612718aa42a7a661adb20c3a487b81224a332d
Bruno Silva Santana <ibrunoss@outlook.com>
2021-11-03 00:08:12 -0300

    update: App component
    - Added MenuItem Messages
    - Added MenuItem Setting
    - Added MenuItem Help
    - Added MenuItem Password
    - Added MenuItem Sign Out
    - Added active state

--- src/App.tsx
+++ src/App.tsx
@@ -37,13 +37,147 @@ const ProfileIcon = (
   </svg>
 );
 
+const MessagesIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
+    />
+  </svg>
+);
+
+const SettingIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+    />
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+    />
+  </svg>
+);
+
+const HelpIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+    />
+  </svg>
+);
+
+const PasswordIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+    />
+  </svg>
+);
+
+const SignOutIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+    />
+  </svg>
+);
+
 export default function App() {
   const [open, setOpen] = useState(false);
+  const [active, setActive] = useState("home");
+
+  const activeClick = (target: string) => () => setActive(target);
   return (
     <>
       <Menu open={open}>
-        <MenuItem label="Home" icon={HomeIcon} />
-        <MenuItem label="Profile" icon={ProfileIcon} />
+        <MenuItem
+          label="Home"
+          icon={HomeIcon}
+          active={active === "home"}
+          onClick={activeClick("home")}
+        />
+        <MenuItem
+          label="Profile"
+          icon={ProfileIcon}
+          active={active === "profile"}
+          onClick={activeClick("profile")}
+        />
+        <MenuItem
+          label="Messages"
+          icon={MessagesIcon}
+          active={active === "messages"}
+          onClick={activeClick("messages")}
+        />
+        <MenuItem
+          label="Setting"
+          icon={SettingIcon}
+          active={active === "setting"}
+          onClick={activeClick("setting")}
+        />
+        <MenuItem
+          label="Help"
+          icon={HelpIcon}
+          active={active === "help"}
+          onClick={activeClick("help")}
+        />
+        <MenuItem
+          label="Password"
+          icon={PasswordIcon}
+          active={active === "password"}
+          onClick={activeClick("password")}
+        />
+        <MenuItem
+          label="Sign Out"
+          icon={SignOutIcon}
+          active={active === "signout"}
+          onClick={activeClick("signout")}
+        />
       </Menu>
       <ToggleMenu open={open} onClick={setOpen} />
     </>
