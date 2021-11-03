0133f965ac263442aea48d2cb07f572e7b706acc
Bruno Silva Santana <ibrunoss@outlook.com>
2021-11-02 21:33:41 -0300

    update: App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,51 @@
+import { useState } from "react";
 import Menu from "./components/navigation/Menu/Menu";
+import MenuItem from "./components/navigation/MenuItem/MenuItem";
+import ToggleMenu from "./components/navigation/ToggleMenu/ToggleMenu";
+
+const HomeIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+    />
+  </svg>
+);
+
+const ProfileIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+    />
+  </svg>
+);
 
 export default function App() {
+  const [open, setOpen] = useState(false);
   return (
-    <div>
-      <h1>Sidebar Menu Using, Html, CSS & JavaScript</h1>
-      <Menu />
-    </div>
+    <>
+      <Menu open={open}>
+        <MenuItem label="Home" icon={HomeIcon} />
+        <MenuItem label="Profile" icon={ProfileIcon} />
+      </Menu>
+      <ToggleMenu open={open} onClick={setOpen} />
+    </>
   );
 }
